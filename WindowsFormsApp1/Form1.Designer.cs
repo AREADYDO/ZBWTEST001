4ecb2b9f8e2e9a7a1a1b1ecd70402fb0afa69c86
鸿鑫 刘 <18476430929@163.com>
2019-09-04 16:29:09 +0800

    001

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,12 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(103, 104);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(654, 104);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(103, 263);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(654, 147);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "订票";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 462);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(894, 505);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace WindowsFormsApp1
 {
@@ -14,6 +16,88 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+           
+    }
+        public static SqlConnection conn;
+        string strConnection = ConfigurationManager.ConnectionStrings["connection"].ToString();//读取配置文件连接信息
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Rows = 0;
+            Random r = new Random();
+            int n = r.Next(100000000, 999999999);
+            textBox2.Text = textBox1.Text;
+            string sql = string.Format("Insert into OrderTest(AirId ,DateTime, AirCode, AirName, StartPoint, EndPoint, AirDate,Price,Site,Card,Name ) values ('{0}','{1}','{2}',N'{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')", textBox2.Text, DateTime.Now.ToString(), textBox1.Text, "中国航空", "北京", "上海", DateTime.Now.ToString(), r.Next(00, 99), r.Next(100, 999), r.Next(100000000, 999999999).ToString()+ r.Next(000000000, 999999999).ToString(),"***");
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(sql, conn);
+                Rows = command.ExecuteNonQuery();
+
+                if (Rows == 1)
+                {
+                    //return true;
+                    label1.Text = "ok!";
+                }
+                else
+                {
+                    // return false;
+                    label1.Text = "err!";
+                }
+            }
+            catch (SqlException ex)
+            {
+                string error = ex.ToString();
+                // return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int Rows = 0;
+            Random r = new Random();
+            int n = r.Next(100000000, 999999999);
+            textBox1.Text = n.ToString();
+            string sql = string.Format("Insert into AirTest(AirId ,DateTime, AirCode, AirName, AirDate, Price, Memo) values ('{0}','{1}','{2}',N'{3}','{4}','{5}','{6}')", textBox1.Text, DateTime.Now.ToString(), textBox1.Text,"中国航空", DateTime.Now.ToString(), r.Next(100, 999), "N");
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(sql, conn);
+                Rows = command.ExecuteNonQuery();
+
+                if (Rows == 1)
+                {
+                    //return true;
+                    label1.Text = "ok!";
+                }
+                else
+                {
+                    // return false;
+                    label1.Text = "err!";
+                }
+            }
+            catch (SqlException ex)
+            {
+                string error = ex.ToString();
+               // return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            conn = new SqlConnection(strConnection);
         }
     }
 }
